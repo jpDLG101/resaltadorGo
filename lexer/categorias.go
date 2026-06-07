3e4aa3f4065132659b6ba8279ee7a9c539c3578c
jpDLG101 <A01563998@tec.mx>
2026-06-07 13:13:52 -0600

    dia7(equipo): revision final y correccion de convenciones

--- lexer/categorias.go
+++ lexer/categorias.go
@@ -6,7 +6,7 @@ import (
 
 func esNumero(s string) bool {
 	for _, r := range s {
-		if !unicode.IsDigit(r){
+		if !unicode.IsDigit(r) {
 			return false
 		}
 	}
@@ -14,21 +14,21 @@ func esNumero(s string) bool {
 }
 
 func esReservadaIF(s string) bool {
-	if s == "if"{
+	if s == "if" {
 		return true
 	}
 	return false
 }
 
 func esReservadaWHILE(s string) bool {
-	if s == "while"{
+	if s == "while" {
 		return true
 	}
 	return false
 }
 
 func esBooleano(s string) bool {
-	if s == "true" || s == "false"{
+	if s == "true" || s == "false" {
 		return true
 	}
 	return false
@@ -37,18 +37,18 @@ func esBooleano(s string) bool {
 func esIdentificador(s string) bool {
 	r := rune(s[0])
 
-	if !unicode.IsLetter(r){
+	if !unicode.IsLetter(r) {
 		return false
 	}
 	for _, r := range s[1:] {
-		if !unicode.IsLetter(r) && !unicode.IsDigit(r){
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
 			return false
 		}
 	}
 	return true
 }
 
-func Clasificar(s string) string{
+func Clasificar(s string) string {
 	if esReservadaIF(s){
 		return "RESERVADA_IF"
 	}
